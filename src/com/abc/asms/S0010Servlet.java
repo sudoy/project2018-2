a582236762a6572b283f064a6891320a9ef74f0b
kimura.arito <kimura.arito@tsd-201511-0004>
2018-06-12 08:50:29 +0900

    売上登録画面(未完成)

--- src/com/abc/asms/S0010Servlet.java
+++ src/com/abc/asms/S0010Servlet.java
@@ -1,18 +1,12 @@
 package com.abc.asms;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import project2.beans.Sales;
-import project2.utils.DBUtils;
 
 @WebServlet("/S0010.html")
 public class S0010Servlet extends HttpServlet {
@@ -31,62 +25,25 @@ public class S0010Servlet extends HttpServlet {
 
 		req.setCharacterEncoding("UTF-8");
 
-		Connection con = null;
-		PreparedStatement ps = null;
-		String sql = null;
-		ResultSet rs = null;
+		String saleDate = req.getParameter("saleDate");
+		String accountId = req.getParameter("accountId");
+		String categoryId = req.getParameter("categoryId");
+		String tradeName = req.getParameter("tradeName");
+		String unitPrice = req.getParameter("unitPrice");
+		String saleNumber = req.getParameter("saleNumber");
+		String note = req.getParameter("note");
 
-		try{
-			//データベースの接続を確立
-			con = DBUtils.getConnection();
+		req.setAttribute("saleDate", saleDate);
+		req.setAttribute("accountId", accountId);
+		req.setAttribute("categoryId", categoryId);
+		req.setAttribute("tradeName", tradeName);
+		req.setAttribute("unitPrice", unitPrice);
+		req.setAttribute("saleNumber", saleNumber);
+		req.setAttribute("note", note);
 
-			sql = "SELECT sale_id = ?, sale_date = ?, account_id = ?, category_id = ?, trade_name = ?, unit_price = ?, sale_number = ?, note = ?"
-					+ " FROM sales;";
-
-			ps = con.prepareStatement(sql);
-
-			//SELECT文にパラメーターの内容をセット
-			ps.setString(1, req.getParameter("saleId"));
-			ps.setString(2, req.getParameter("saleDate"));
-			ps.setString(3, req.getParameter("accountId"));
-			ps.setString(4, req.getParameter("categoryId"));
-			ps.setString(5, req.getParameter("tradeName"));
-			ps.setString(6, req.getParameter("unitPrice"));
-			ps.setString(7, req.getParameter("saleNumber"));
-			ps.setString(8, req.getParameter("note"));
-
-			//SELECT命令を実行
-			rs = ps.executeQuery();
-
-			//ResultSet→JavaBeansに変換する
-			rs.next();
-
-			Sales sales = new Sales(rs.getInt("sale_id"),
-							rs.getDate("sale_date"),
-							rs.getInt("account_id"),
-							rs.getInt("category_id"),
-							rs.getString("trade_name"),
-							rs.getInt("unit_price"),
-							rs.getInt("sale_number"),
-							rs.getString("note")
-							);
-
-			req.setAttribute("sales", sales);
-
-			//JSPへフォワード
-			getServletContext().getRequestDispatcher("/WEB-INF/s0011.jsp")
-				.forward(req, resp);
-
-		}catch(Exception e){
-			throw new ServletException(e);
-
-		}finally{
-			try{
-				DBUtils.close(rs);
-				DBUtils.close(ps);
-				DBUtils.close(con);
-			}catch(Exception e){}
-		}
+		//JSPへフォワード
+		getServletContext().getRequestDispatcher("/WEB-INF/s0011.jsp")
+			.forward(req, resp);
 
 	}
 
--- src/com/abc/asms/S0011Servlet.java
+++ src/com/abc/asms/S0011Servlet.java
@@ -20,7 +20,6 @@ public class S0011Servlet extends HttpServlet {
 			throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 
-		String saleId = req.getParameter("saleId");
 		String saleDate = req.getParameter("saleDate");
 		String accountId = req.getParameter("accountId");
 		String categoryId = req.getParameter("categoryId");
@@ -36,21 +35,20 @@ public class S0011Servlet extends HttpServlet {
 		try {
 			con = DBUtils.getConnection();
 
-			sql = "INSERT INTO sales(sale_id, sale_date, account_id, category_id, trade_name, unit_price, sale_number, note)"
+			sql = "INSERT INTO sales(sale_date, account_id, category_id, trade_name, unit_price, sale_number, note)"
 					+ "values(?,?,?,?,?,?,?,?)";
 
 			//INSERT命令の準備
 			ps = con.prepareStatement(sql);
 
 			//INSERT命令にポストデータの内容をセット
-			ps.setString(1, saleId);
-			ps.setString(2, saleDate);
-			ps.setString(3, accountId);
-			ps.setString(4, categoryId);
-			ps.setString(5, tradeName);
-			ps.setString(6, unitPrice);
-			ps.setString(7, saleNumber);
-			ps.setString(8, note);
+			ps.setString(1, saleDate);
+			ps.setString(2, accountId);
+			ps.setString(3, categoryId);
+			ps.setString(4, tradeName);
+			ps.setString(5, unitPrice);
+			ps.setString(6, saleNumber);
+			ps.setString(7, note);
 
 			ps.executeUpdate();
 
