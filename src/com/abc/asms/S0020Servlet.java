ccff2521be1aeedf72f4a989ccd41b6bc4eb15ff
nojima <nojima@DESKTOP-SR8NC1P>
2018-06-19 11:42:12 +0900

    売上検索（キャンセル対応）アカウント検索（キャンセル対応）

--- src/com/abc/asms/S0020Servlet.java
+++ src/com/abc/asms/S0020Servlet.java
@@ -1,9 +1,6 @@
 package com.abc.asms;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,19 +16,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.abc.asms.beans.S0021;
-import com.abc.asms.utils.DBUtils;
+import com.abc.asms.beans.SearchSale;
 import com.abc.asms.utils.DBUtils2;
-import com.abc.asms.utils.Utils;
 @WebServlet("/S0020.html")
 public class S0020Servlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// ログインチェック
-		if(!Utils.checkLogin(req, resp)) {
-			return;
-		}
+//		if(!Utils.checkLogin(req, resp)) {
+//			return;
+//		}
 		req.setCharacterEncoding("UTF-8");
 		LocalDateTime d = LocalDateTime.now();
 		String today = DateTimeFormatter.ofPattern("yyyy/MM/dd").format(d);
@@ -49,17 +44,10 @@ public class S0020Servlet extends HttpServlet {
 
 		req.setCharacterEncoding("UTF-8");
 		HttpSession session = req.getSession();
-		Connection con = null;
-		PreparedStatement ps = null;
-		String sql = null;
-		ResultSet rs = null;
-
-		String note = req.getParameter("note");
-		String tradeName = req.getParameter("trade_name");
 		String saleDate1 = req.getParameter("sale_date1");
 		String saleDate2 = req.getParameter("sale_date2");
 		String accountName = req.getParameter("name");
-		String categoryName[] = req.getParameterValues("categoryName");
+
 
 		DBUtils2.getConnection2(req, resp);
 
@@ -70,112 +58,15 @@ public class S0020Servlet extends HttpServlet {
 			.forward(req, resp);
 			return;
 		}
-		try{
-			con = DBUtils.getConnection();
 
-			//SQL
-			sql = "select sale_id,sale_date,a.name,c.category_name, s.trade_name, s.unit_price, s.sale_number,note from accounts a left join sales s on a.account_id = s.account_id "
-					+"left join categories c on c.category_id = s.category_id "
-					+ "where 0 = 0 ";
+		SearchSale ss = new SearchSale(req.getParameter("sale_date1"),req.getParameter("sale_date2"),
+				req.getParameter("name"),req.getParameterValues("categoryName"),req.getParameter("trade_name"),
+				req.getParameter("note"));
+		session.setAttribute("ss", ss);
 
-			//備考検索
-			if(note != "") {
-				sql += " and note like '%" + note + "%'";
-			}else {
-				sql += "";
-			}
-
-			//商品名検索
-			if(tradeName != "") {
-				sql += " and trade_name like '%" + tradeName + "%'";
-			}else {
-				sql += "";
-			}
-
-			//日付検索
-			if(!saleDate1.equals("") && !saleDate2.equals("")) {
-				sql += " and sale_date between '" + saleDate1 + "' and '" + saleDate2+ "'";
-
-			}else if(saleDate1.equals("") && !saleDate2.equals("")){
-				sql += " and sale_date <= '" + saleDate2 + "'";
-
-			}else if(!saleDate1.equals("") && saleDate2.equals("")) {
-				sql += " and sale_date >= '" + saleDate1 + "'";
-
-			}else {
-				sql += "";
-			}
-
-			//担当検索
-			if(accountName !=  "") {
-				sql += " and name =" + "'"+accountName+"'";
-			}else {
-				sql += "";
-			}
-			//カテゴリー検索
-			if(req.getParameterValues("categoryName") == null) {
-				sql += "";
-			}
-			if(categoryName != null) {
-				sql += "and category_name in(";
-				for(int i = 0; i < categoryName.length; i++) {
-					if(i != categoryName.length-1) {
-						sql += "'"+ categoryName[i] +"'"+",";
-					}else {
-						sql += "'"+ categoryName[i] +"'"+")";
-					}
-				}
-
-			}
-
-			//id降順
-			sql += "order by sale_id";
-
-			//SELECT命令の準備
-			ps = con.prepareStatement(sql);
-			System.out.println(ps);
-			//SELECT命令を実行
-			rs = ps.executeQuery();
+		resp.sendRedirect("S0021.html");
 
 
-			List<S0021> list = new ArrayList<>();
-			while(rs.next()) {
-				S0021 sale = new S0021(rs.getInt("sale_id"),
-						rs.getDate("sale_date"),
-						rs.getString("name"),
-						rs.getString("category_name"),
-						rs.getString("trade_name"),
-						rs.getInt("unit_price"),
-						rs.getInt("sale_number"),
-						rs.getString("note"));
-
-				list.add(sale);
-			}
-			if(list.isEmpty()) {
-				errors = new ArrayList<>();
-				errors.add("検索結果はありません。");
-				session.setAttribute("errors", errors);
-				getServletContext().getRequestDispatcher("/WEB-INF/s0020.jsp")
-						.forward(req, resp);
-				return;
-			}
-			//JavaBeansをJSPへ渡す
-			req.setAttribute("list", list);
-
-			//foward
-			getServletContext().getRequestDispatcher("/WEB-INF/s0021.jsp")
-				.forward(req, resp);
-		}catch(Exception e){
-			throw new ServletException(e);
-		}finally{
-			//終了処理
-			try{
-				DBUtils.close(rs);
-				DBUtils.close(ps);
-				DBUtils.close(con);
-			}catch(Exception e){
-			}
-		}
 	}
 	private List<String> validate(String saleDate1,String saleDate2,HttpServletRequest req,String accountName) {
 		List<String> errors = new ArrayList<>();
---
+++ src/com/abc/asms/beans/SearchSale.java
@@ -0,0 +1,62 @@
+package com.abc.asms.beans;
+
+public class SearchSale {
+
+	private String saleDate1;
+	private String saleDate2;
+	private String accountName;
+	private String[] categoryName;
+	private String tradeName;
+	private String note;
+	public SearchSale( String saleDate1,String saleDate2, String accountName, String[] categoryName, String tradeName, String note) {
+		super();
+
+		this.saleDate1 = saleDate1;
+		this.saleDate2 = saleDate2;
+		this.accountName = accountName;
+		this.categoryName = categoryName;
+		this.tradeName = tradeName;
+		this.note = note;
+
+	}
+
+	public String getSaleDate1() {
+		return saleDate1;
+	}
+	public void setSaleString1(String saleDate1) {
+		this.saleDate1 = saleDate1;
+	}
+	public String getSaleDate2() {
+		return saleDate2;
+	}
+	public void setSaleDate2(String saleDate2) {
+		this.saleDate2 = saleDate2;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String[] getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String[] categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getTradeName() {
+		return tradeName;
+	}
+	public void setTradeName(String tradeName) {
+		this.tradeName = tradeName;
+	}
+
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+
+}
