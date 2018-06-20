4e25e91949d16c1459fc4fe5f9de2dd599c4ec95
nojima <nojima@DESKTOP-SR8NC1P>
2018-06-20 09:59:41 +0900

    新しいsql文

--- sql/accounts.sql
+++ sql/accounts.sql
@@ -7,6 +7,11 @@ create table accounts(
 );
 
 insert into accounts (account_id, name, mail, password, authority)values
-(1, 'イチロー', 'ichiro@sak.com', MD5('ichiro@sak.com'), 1),
-(2, 'ダルビッシュ 有', 'yu.dalvish@sak.com', MD5('yu.dalvish@sak.com'), 1),
-(3, '田中将大', 'masahiro.tanaka@sak.com', MD5('masahiro.tanaka@sak.com'), 0);
+(1, 'Admin', 'yuichi.sudo@ssie.jp', MD5('0000'), 10),
+(2, '土樋奎太', 'tsuchitoi.keita@mail.tokyosystem.co.jp', MD5('0000'), 1),
+(3, '笹原颯', 'sasahara.hayate@mail.tokyosystem.co.jp', MD5('0000'), 1),
+(4, '中村楓佳', 'nakamura.fuka@mail.tokyosystem.co.jp', MD5('0000'), 1),
+(5, '阪本真由', 'sakamoto.mayu@mail.tokyosystem.co.jp', MD5('0000'), 1),
+(6, '叶内森', 'kanauchi.shin@mail.tokyosystem.co.jp', MD5('0000'), 1),
+(7, '野島紳司', 'nojima.shinji@mail.tokyosystem.co.jp', MD5('0000'), 1),
+(8, '木村有理人', 'kimura.arito@mail.tokyosystem.co.jp', MD5('0000'), 1);
