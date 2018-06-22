626a559abd83bc796dd561022b9a001620fa9d73
kanauchi <kanauchi@tsd-201601-0009>
2018-06-22 13:09:35 +0900

    kana:ログアウト

--- src/com/abc/asms/C0030Servlet.java
+++ src/com/abc/asms/C0030Servlet.java
@@ -2,6 +2,7 @@ package com.abc.asms;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 @WebServlet("/C0030.html")
 public class C0030Servlet extends HttpServlet {
 	@Override
@@ -17,13 +19,28 @@ public class C0030Servlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		HttpSession session = req.getSession();
-		session.setAttribute("accounts", null);
-		//ログアウトメッセージ
-		List<String> successes = new ArrayList<>();
-		successes.add("ログアウトしました。");
-		session.setAttribute("successes", successes);
-		// リダイレクト
-		resp.sendRedirect("C0010.html");
-	}
 
+		Enumeration<String> e = session.getAttributeNames();
+		String key = null;
+		while (e.hasMoreElements()) {
+			key = (String) e.nextElement();
+			System.out.println(key);
+			if (key.contains("accounts")) {
+				session.setAttribute("accounts", null);
+				
+				//ログアウトメッセージ
+				List<String> successes = new ArrayList<>();
+				successes.add("ログアウトしました。");
+				session.setAttribute("successes", successes);
+				
+				// リダイレクト
+				resp.sendRedirect("C0010.html");
+				return;
+			}
+		}
+
+		getServletContext().getRequestDispatcher("/WEB-INF/c0010.jsp")
+				.forward(req, resp);
+
+	}
 }
